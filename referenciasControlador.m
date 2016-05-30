48a732b7f0b68aa6fa9892bf93dff5ee88d527d8
Lucas Serrano <lucas.serrano@cas-sistemas.com.br>
2016-05-30 00:12:26 -0300

    Mudanças ao arquivo de referência.

--- referenciasControlador.m
+++ referenciasControlador.m
@@ -1,5 +1,6 @@
 clear;
 % load('data');
+%% 
 
 kc = 0.2371;
 pc = 0.1;
@@ -10,13 +11,27 @@ taum = 0.5015;
 Kenc = 3.18;
 Km = pi;
 
-% tempo = linspace(0, max(size(pos))*0.01, max(size(pos)))';
-% 
-% vel = [tempo vel];
-% 
-% pos = [tempo pos];
-% 
-% in = [tempo in];
+%% 
+
+tempo = linspace(0, max(size(pos))*0.01, max(size(pos)))';
+ 
+vel = [tempo vel];
+ 
+pos = [tempo pos];
+ 
+in = [tempo in];
+
+%% 
+
+tempo = linspace(0, max(size(vel))*0.01, max(size(vel)))';
+
+pos=pos(2:end,:);
+
+vel=vel(2:end,:);
+
+in=in(2:end,:);
+
+%% 
 
 Gma = tf(Km*Kenc,[taum 1 0]);
 
@@ -27,17 +42,3 @@ step(Gmf);
 Ts = 0.8*3.59;
 
 % sisotool(Gma)
-
-tempo = linspace(0, max(size(vel))*0.01, max(size(vel)))';
-
-pos=pos(2:end,:);
-
-vel=vel(2:end,:);
-
-in=in(2:end,:);
-
-vel = [tempo vel];
-
-pos = [tempo pos];
- 
-in = [tempo in];
